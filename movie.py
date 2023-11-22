0854316ec9adef642f41629c3182bc8cf088ac28
verkor49@gmail.com <verkor49@gmail.com>
2023-11-22 14:00:45 +0200

    add first python lab code

---
+++ movie.py
@@ -0,0 +1,18 @@
+movie = "Spider_Man"
+print(movie)
+
+
+is_employee = False
+is_employer = True
+
+print(is_employee)
+print(is_employer)
+
+
+is_sunny = not True
+print(is_sunny)
+
+is_raining = not False
+print(is_raining)
+
+
